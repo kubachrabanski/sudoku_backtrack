ee6ba2b270e670ab655cbbf641a4bd66283c3da4
kubachrabanski <kuba.chrabanski@gmail.com>
2019-07-09 18:32:01 +0200

    Initial clean up and setup

--- bin/run.ml
+++ bin/run.ml
@@ -1,9 +1,2 @@
-open Lib
-
-let rec read_lines a =
-    try read_lines ((read_line ()) :: a)
-    with End_of_file -> List.rev a
-
-let () =
-    read_lines [] |> Sudoku.of_lines_string |> Backtrack.run |> Sudoku.print
+let () = ()
 
